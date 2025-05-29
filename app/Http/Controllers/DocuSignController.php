b961dbadec69e1401a47a9c67ea2deced1c89777
malashreedhun <MDhungel@irafinancial.com>
2025-05-29 14:07:24 -0500

    redirection successful

--- app/Http/Controllers/DocuSignController.php
+++ app/Http/Controllers/DocuSignController.php
@@ -16,15 +16,15 @@ class DocuSignController extends Controller
 
     public function send(Request $request)
     {
-         $name  = $request->signer_name;
-        $email = $request->signer_email;
+        $name  = $request->input('signer_name');
+        $email = $request->input('signer_email');
 
-
-        // JWT Assertion for OAuth
-        $baseUrl = 'https://demo.docusign.net/restapi';
-        $integratorKey = env('DOCUSIGN_INTEGRATION_KEY');
-        $userId        = env('DOCUSIGN_USER_ID');
-        $privateKey    = storage_path('app/docusign_private.key');
+        //
+        // 1) JWT Assertion for OAuth
+        //
+        $integratorKey   = env('DOCUSIGN_INTEGRATION_KEY');
+        $userId          = env('DOCUSIGN_USER_ID');
+        $privateKeyPath  = storage_path('app/docusign_private.key');
         $jwtPayload = [
             'iss'   => $integratorKey,
             'sub'   => $userId,
@@ -32,72 +32,64 @@ class DocuSignController extends Controller
             'exp'   => time() + 3600,
             'scope' => 'signature impersonation',
         ];
-        $jwt = JWT::encode($jwtPayload, file_get_contents($privateKey), 'RS256');
-        $client = new Client ([
-            'verify' => false, // Disable SSL verification for testing (not recommended in production)
+        $jwt = JWT::encode($jwtPayload, file_get_contents($privateKeyPath), 'RS256');
+
+        $client = new Client([
+            'verify' => false, // disable SSL verification in dev
         ]);
-        // 1) Get access token
-        $authRes = $client->post("https://account-d.docusign.com/oauth/token", [
+
+        $authRes = $client->post('https://account-d.docusign.com/oauth/token', [
             'form_params' => [
                 'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
                 'assertion'  => $jwt,
             ],
         ]);
 
-
-        $accessToken = json_decode($authRes->getBody(), true)['access_token'];
+        $accessToken = data_get(json_decode($authRes->getBody(), true), 'access_token');
         if (!$accessToken) {
             return redirect()->back()->withErrors(['error' => 'Failed to obtain access token']);
         }
 
+        //
         // 2) Build envelope definition
-        $pdfBase64 = base64_encode(file_get_contents(storage_path('app/myfile.pdf')));
+        //
+        $pdfContent = file_get_contents(storage_path('app/myfile.pdf'));
         $envelopeDef = [
             'emailSubject'       => 'Please sign your Authorization Form',
-            'envelopeIdStamping' => 'true',            // stamps envelope ID on PDF  //documents: $pdfBase64,
-            'documents' => [[
-                'documentBase64' => base64_encode(
-                    file_get_contents(storage_path('app/myfile.pdf'))
-                ),
-                'name'           => 'Authorization.pdf',
-                'fileExtension'  => 'pdf',
-                'documentId'     => '1',
-            ]],
+            'emailBlurb'         => 'Please review and sign the attached form.',
+            'envelopeIdStamping' => true,
+            'documents' => [
+                [
+                    'documentBase64' => base64_encode($pdfContent),
+                    'name'           => 'Authorization.pdf',
+                    'fileExtension'  => 'pdf',
+                    'documentId'     => '1',
+                ],
+            ],
             'recipients' => [
-                'signers' => [[
-                    'email'        => $email,
-                    'name'         => $name,
-                    'recipientId'  => '1',
-                    'routingOrder' => '1',]],],
-                    // 'clientUserId' => '1000',           // embedded signing
-
-                    // 'tabs' => [
-                    //     // Prefill their name
-                    //     'textTabs' => [[
-                    //         'tabLabel'   => 'SignerName',
-                    //         'value'      => $name,
-                    //         'documentId' => '1',
-                    //         'pageNumber' => '1',
-                    //         'xPosition'  => '100',
-                    //         'yPosition'  => '150',
-                    //     ]],
-                    //     // Signature box at anchor
-                    //     'signHereTabs' => [[
-                    //         'anchorString'  => '/sign_here/',
-                    //         'anchorUnits'   => 'pixels',
-                    //         'anchorXOffset' => '0',
-                    //         'anchorYOffset' => '0',
-            'status' => 'sent', // 'created' for draft, 'sent' to send immediately
-
-
+                'signers' => [
+                    [
+                        'email'        => $email,
+                        'name'         => $name,
+                        'recipientId'  => '1',
+                        'routingOrder' => '1',
+                        'clientUserId' => '1000', // embedded signing
+                    ],
+                ],
+            ],
+            'status' => 'sent',
         ];
 
         $accountId = env('DOCUSIGN_ACCOUNT_ID');
         if (!$accountId) {
             return redirect()->back()->withErrors(['error' => 'DocuSign account ID not configured']);
         }
-        $baseUrl   = 'https://demo.docusign.net/restapi';
-        // 3) Create envelope
+
+        $baseUrl = 'https://demo.docusign.net/restapi';
+
+        //
+        // 3) Create the envelope
+        //
         $envRes = $client->post("$baseUrl/v2.1/accounts/$accountId/envelopes", [
             'headers' => [
                 'Authorization' => "Bearer $accessToken",
@@ -105,100 +97,103 @@ class DocuSignController extends Controller
             ],
             'json' => $envelopeDef,
         ]);
-        $envelopeId = json_decode($envRes->getBody(), true)['envelopeId'];
 
-        return response()-> json([
-            'success' => true,
+        $envelopeId = data_get(json_decode($envRes->getBody(), true), 'envelopeId');
+
+        //
+        // 4) Create the embedded signing view
+        //
+        $viewRequest = [
+            'returnUrl'            => route('dashboard.authorization.callback', ['envelope_id' => $envelopeId]),
+            'authenticationMethod' => 'none',
+            'email'                => $email,
+            'userName'             => $name,
+            'clientUserId'         => '1000',
+        ];
+
+        try {
+            $viewRes = $client->post(
+                "$baseUrl/v2.1/accounts/$accountId/envelopes/$envelopeId/views/recipient",
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer $accessToken",
+                        'Content-Type'  => 'application/json',
+                    ],
+                    'json' => $viewRequest,
+                ]
+            );
+
+            $signingUrl = data_get(json_decode($viewRes->getBody(), true), 'url');
+        } catch (\Exception $e) {
+            // Log or inspect the error
+            return response()->json([
+                'success' => false,
+                'error'   => $e->getMessage(),
+            ], 500);
+        }
+
+        //
+        // 5) Return the envelope ID + signing URL
+        //
+        return response()->json([
+            'success'    => true,
             'envelopeId' => $envelopeId,
+            'signingUrl' => $signingUrl,
         ]);
-    }}
+    }
 
-        // // 4) Create embedded recipient view (signing URL)
-        // $viewReq = [
-        //     'returnUrl'            => route('dashboard.authorization.callback', ['envelope_id' => $envelopeId]),
-        //     'authenticationMethod' => 'none',
-        //     'email'                => $email,
-        //     'userName'             => $name,
-        //     'clientUserId'         => '1000',
-        // ];
-        // $viewRes = $client->post(
-        //     "$baseUrl/v2.1/accounts/$accountId/envelopes/$envelopeId/views/recipient",
-        //     ['headers'=>['Authorization'=>"Bearer $accessToken"], 'json'=>$viewReq]
-        // );
-        // $signingUrl = json_decode($viewRes->getBody(), true)['url'];
+    public function callback(Request $request)
+    {
+        $envelopeId = $request->query('envelope_id');
+        $accountId  = env('DOCUSIGN_ACCOUNT_ID');
+        $accessToken = $this->getAccessToken();
 
-        // // 5) Redirect user into DocuSign signing ceremony
-        // return redirect()->away($signingUrl);
+        $client = new Client([
+            'headers' => ['Authorization' => "Bearer $accessToken"],
+            'verify'  => false,
+        ]);
 
-//new new new new new new new new new new new new new new new new new new new
+        $baseUrl = 'https://demo.docusign.net/restapi';
 
+        //
+        // 6) Fetch the completed, combined PDF
+        //
+        $res = $client->get("$baseUrl/v2.1/accounts/$accountId/envelopes/$envelopeId/documents/combined", [
+            'stream' => true,
+        ]);
 
+        //
+        // 7) Stream it back inline
+        //
+        return response($res->getBody(), 200)
+            ->header('Content-Type', 'application/pdf')
+            ->header('Content-Disposition', "inline; filename=\"signed-{$envelopeId}.pdf\"");
+    }
 
+    protected function getAccessToken()
+    {
+        $integratorKey   = env('DOCUSIGN_INTEGRATION_KEY');
+        $userId          = env('DOCUSIGN_USER_ID');
+        $privateKeyPath  = storage_path('app/docusign_private.key');
+        $jwtPayload = [
+            'iss'   => $integratorKey,
+            'sub'   => $userId,
+            'aud'   => 'account-d.docusign.com',
+            'exp'   => time() + 3600,
+            'scope' => 'signature impersonation',
+        ];
+        $jwt = JWT::encode($jwtPayload, file_get_contents($privateKeyPath), 'RS256');
 
-//everything below this line is for embedded signing
+        $client = new Client([
+            'verify' => false,
+        ]);
+        $res = $client->post('https://account-d.docusign.com/oauth/token', [
+            'form_params' => [
+                'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'assertion'  => $jwt,
+            ],
+        ]);
 
-//         // after $envelopeId is set
-//         $viewDef = [
-//         'returnUrl'            => route('dashboard.authorization.callback', ['envelope_id' => $envelopeId]),
-//         'authenticationMethod' => 'none',
-//         'email'                => $email,
-//         'userName'             => $name,
-//         'clientUserId'         => '1000',
-//         ];
-//         $viewRes = $client->post(
-//         "{$baseUrl}/v2.1/accounts/{$accountId}/envelopes/{$envelopeId}/views/recipient",
-//         ['json' => $viewDef]
-//         );
-//         $signingUrl = json_decode($viewRes->getBody(), true)['url'];
-
-//         return Inertia::location($signingUrl);
-//     }
-
-//     public function callback(Request $request)
-//     {
-//         $envelopeId = $request->query('envelope_id');
-//         $accountId  = env('DOCUSIGN_ACCOUNT_ID');
-
-//         $client = new Client([
-//             'headers' => ['Authorization'=>"Bearer ". $this->getAccessToken()],
-//             'verify'  => false, // Disable SSL verification for testing (not recommended in production)
-//         ]);
-//         $baseUrl = 'https://demo.docusign.net/restapi';
-
-//         // 6) Fetch the completed, stamped PDF
-//         $res = $client->get("$baseUrl/v2.1/accounts/$accountId/envelopes/$envelopeId/documents/combined", [
-//             'stream' => true,
-//         ]);
-
-//         // 7) Stream it back to browser
-//         return response($res->getBody(), 200)
-//             ->header('Content-Type', 'application/pdf')
-//             ->header('Content-Disposition', "inline; filename=\"signed-{$envelopeId}.pdf\"");
-//     }
-
-//     protected function getAccessToken()
-//     {
-//         // (Extract your JWT/OAuth code into here for reuse in callback)
-//         $integratorKey = env('DOCUSIGN_INTEGRATOR_KEY');
-//         $userId        = env('DOCUSIGN_USER_ID');
-//         $privateKey    = storage_path('app/docusign_private.key');
-//         $jwtPayload = [
-//             'iss'   => $integratorKey,
-//             'sub'   => $userId,
-//             'aud'   => 'account-d.docusign.com',
-//             'exp'   => time() + 3600,
-//             'scope' => 'signature impersonation',
-//         ];
-//         $jwt = JWT::encode($jwtPayload, file_get_contents($privateKey), 'RS256');
-//         $client = new Client([
-//             'verify' => false, // Disable SSL verification for testing (not recommended in production)
-//         ]);
-//         $res = $client->post("https://account-d.docusign.com/oauth/token", [
-//             'form_params' => [
-//                 'grant_type' => 'urn:ietf:params:oauth:grant-type:jwt-bearer',
-//                 'assertion'  => $jwt,
-//             ],
-//         ]);
-//         return json_decode($res->getBody(), true)['access_token'];
-//     }
-// }
+        return data_get(json_decode($res->getBody(), true), 'access_token');
+    }
+}
